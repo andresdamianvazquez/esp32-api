ab6428d5b6b83afb79fabb4eee32068acf7b7051
Andres <andres.vazquez@geoagris.com>
2025-06-15 00:57:56 -0300

    cambio en run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto por variable de entorno
+    app.run(host="0.0.0.0", port=port)        # Escuchar en todas las interfaces
